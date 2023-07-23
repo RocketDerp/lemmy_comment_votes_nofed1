b67a14e4f084718796911c822c10a5096e0f1bca
RocketDerp <113625597+RocketDerp@users.noreply.github.com>
2023-07-22 20:01:26 -0700

    Saturday work

--- api_tests/src/remote_to_remote.spec.ts
+++ api_tests/src/remote_to_remote.spec.ts
@@ -1,5 +1,5 @@
 /*
-   Grand Tour
+   remote_to_remote - from The North Pole to The South Pole, where the communuity is homed at the equator.
    In July 2023, Lemmy is dealing with the aftermath of explosive growth to over 1,300 instances (servers).
    Previous testing was mostly focused on a subscribing intance (alpha) making sure it updated the home instance (beta) of a community.
    This takes a more holistic approach:
@@ -9,6 +9,9 @@
       Making sure moderators on remote instances can do actions that reach all the subscribed instances.
 
    ToDo: have an admin API call to induce outbound federation failures such as an Internet outage or unreachable peer instance.
+   ToDo: down server, blinking server availability, testing. We need API to fail federation outbound.
+         take connection down, create post, bring connection up, create comment on post, race condition?
+         severs all up, create post on alpha on beta community, take down alpha, moderatoe post on gamma, bring up alpha
 */
 jest.setTimeout(180000);
 
@@ -65,13 +68,30 @@ beforeAll(async () => {
 afterAll(async () => {
 });
 
+test("create non-admin end-user login all instances", async () => {
+  // await multi.createNonAdminUsersAllInstances();
+  expect(true).toBe(false);
+});
 
-test("create community", async () => {
+test("create community 'myfirst' with admin login on beta", async () => {
+  await multi.createCommunityOnBeta("myfirst");
+});
+
+test("follow 'myfirst' community and validate on all instances", async () => {
+  await multi.followValidateBetaCommunity();
+});
+
+test("create community 'grandtour' with end-user login", async () => {
+  // switchuser call?
   await multi.createCommunityOnBeta("grandtour");
 });
 
-test("follow community and validate", async () => {
+test("follow 'grandtour' community and validate on all instances", async () => {
   await multi.followValidateBetaCommunity();
+});
+
+test("list community and compare all instances", async () => {
+  expect(true).toBe(false);
 });
 
 test("Internet test (create a post with url link)", async () => {
@@ -92,7 +112,8 @@ test("Edit the comment created in previous test", async () => {
 });
 
 test("upvote comment that was edited in previous test", async () => {
-  await multi.commentUpvoteOnAlpha(2);
+  // the same user upvoting their own comment, won't increase score
+  await multi.commentUpvoteOnAlpha(1);
 });
 
 test("Delete the upvoted comment", async () => {
@@ -103,26 +124,128 @@ test("Undelete the deleted comment", async () => {
   await multi.undeleteCommentOnAlpha();
 });
 
-test("add a new comment to the same post, ensure count of commenbts equal", async () => {
+test("add new comment to the same post, ensure count of comments equal", async () => {
   await multi.createCommentOnAlphaPost(2);
 });
 
 test("admin comment removal of just-added comment", async () => {
+  // NOTE: change in 0.18.3 behavior is intentional
+  //       https://github.com/LemmyNet/lemmy/issues/3652#issuecomment-1645696038
   await multi.setFocusGammaCommentFromAlpha();
   await multi.adminCommentRemovalFromGamma();
 });
 
 test("undo admin comment removal", async () => {
+  expect(true).toBe(false);
 });
 
+test("instance admin powers spanning instances, viewing deleted content", async () => {
+  // Many people have established in ther mind the concept of an "admin" from monolytic system
+  //   of Reddit-like communities. With federation in the Lemmy version 0.18 era, an admin has no powers
+  //   outside their own instance. Or do they? What are expectations, and this testing code serves
+  //   to try and make sure changes in behavior are documented by testing said behaviors.
+  //
+  // The post.spec.ts "Delete a post" does venture into federation testing:
+  //   https://github.com/LemmyNet/lemmy/blob/102124b6d29edd7d799ee12a6f6df05dbbbf4f6c/api_tests/src/post.spec.ts#L246C14-L246C14
+  // But it also does what in July 2023 most of the tests in this suite do, it uses accounts
+  //   that are admins of servers. Should an admin be able to view a tombstone of a post?
+  //   Is that a backend lemmy API restriction or a front-end lemmy-ui level behavior choice?
+  //   Changes between 0.18.2 and 0.18.3 are such that posts are no longer returned by this API and
+  //      that choice to show user-deleted and moderator-removed posts and comments is no longer in the hands
+  //      of lemmy-ui or other API clients.
+  //   Further, in some of the established tests in 0.18.2 - attention isn't given to which account
+  //      created the community being tested, and would the moderator of a community be able to fetch
+  //      a post that was admin-removed or moderator-removed?
+  //      Often the tests are done with a simplistic asumption that a getPost() function call by the API
+  //      client is being done as a kind of anonymous end-user, but that's not what is happening.
+  //      This remote_to_remote federation is concerned to examine actual behavior with testing.
+  expect(true).toBe(false);
+});
+
+test("behaviors spanning instances, banned from community remove-data option", async () => {
+  // the ban from community call has a data-remove option
+  // create a user, create content on all 3 servers, try with and without option.
+  expect(true).toBe(false);
+});
+
+
+test("behaviors spanning instances, a user removes their account", async () => {
+  // In Lemmy version 0.18.2 - a user who deletes their own comment or post has the option
+  //   to undelete/undo/restore that content.
+  //   Deleting the entire account is supposed to, in real-time, purge comment and post content.
+  //   (NOTEWORTHY: Sites like Facebook oftgen have a grace period due to end-users in social conflict
+  //      making rash choices and wanting their account restored the next day)
+  // Another situation is federation subscription to communities
+  //   Create a new end-user on alpha, "bright_candle_a"
+  //   have beta establish a new beta community "beta_new_a"
+  //   subscribe "bright_candle_a" on alpha to the "beta_new_a" community remotely. They will be sole subscriber on alpha to "beta_new_a"
+  //   Create a new post and comment in "beta_new_a" on beta
+  //   delete the newly created end-user on alpha, without unsubscribing from "beta_new_a" first
+  //   create a new post and comment in "beta_new_a" on beta
+  //   Study the community on alpha if new content delivered.
+  // This code aims to demonstrate how Lemmy behaves with testing code.
+  expect(true).toBe(false);
+});
+
+test("behaviors spanning instances, delete of an entire community", async () => {
+  // Masive database transactions and social impact
+  expect(true).toBe(false);
+});
+
+test("behaviors spanning instances, delete of an entire instance", async () => {
+  // Masive database transactions and social impact
+  expect(true).toBe(false);
+});
+
+
+test("behaviors spanning instances, federation outbound when subscriber count reaches zero", async () => {
+  // create a function to collect count of posts in a specific community on all servers
+  // create a function to collect count of comments in a specific ommunity on all servers
+  // Create a new community on beta "beta_new_b"
+  // Follow that community on alpha, 1 subscriber on gamma
+  // Create a post on alpha, create a comment on beta
+  // Follow that community on gamma, 1 subscriber on gamma
+  // Create a post on alpha, create a comment on gamma
+  // Create a post on gamma
+  // Unfollow on gamma, zero subscribers on gamma
+  // Create a post on gamma
+  // Create a comment on gamma
+  // Create a post on alpha
+  // Unfollow on alpha, zero subscribers on alpha
+  // Create a post on alpha
+  // Create a post on beta
+  expect(true).toBe(false);
+});
+
+test("behaviors spanning instances, recycle of names", async () => {
+  // what happens after user deletes an account on an instance, can same username be picked again?
+  // what happens after a community is deleted, can same community be picked again?
+  // API clients who cash id numbers for community and users...?
+  expect(true).toBe(false);
+});
 
 /*
 ToDo: consider counting the reports on all 3 servers before adding the new one
       previous tests might have already left things inconsistent?
 */
-test("create new post and comment, report the comment", async () => {
+test("report the comment", async () => {
   await multi.reportBetaCommentOnAlpha();
 });
+
+test("resolve report on the comment", async () => {
+  // beta is moderator of community, that's easiest to start with for actor?
+  // ToDo: test resolving the report on gamma, then validated resolve status on alpha
+  expect(true).toBe(false);
+});
+
+test("comment reply notification from two remote instances", async () => {
+  // community is homed on beta, which requires alpha to traverse beta to reach gamma
+  // create a comment on alpha, then reply to comment on gamma, hitting all 3 instances
+  // also try to block the community from alpha, create another reply on gamma, see if notificaiton blocked too.
+  //   see issue: https://github.com/LemmyNet/lemmy/issues/3684
+  expect(true).toBe(false);
+});
+
 
 test("establish community with remote moderator", async () => {
   /*
@@ -140,4 +263,11 @@ test("establish community with remote moderator", async () => {
   12. alphamod0 community feature a post from gamauser0
   13. alphamod0 lock a post from gamauser0
   */
+  expect(true).toBe(false);
+});
+
+test("spot-check some unauthenticated user behaviors", async () => {
+  // probably needs own config file, but this spans posts and comments, so central code here is of use.
+  // add a test for: https://github.com/LemmyNet/lemmy/pull/3689
+  expect(true).toBe(false);
 });

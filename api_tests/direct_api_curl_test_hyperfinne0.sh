90d87e3de8ab1739f252a73f5ee381e352b94661
RocketDerp <113625597+RocketDerp@users.noreply.github.com>
2023-09-01 13:38:24 -0700

    hyperfine script filename

--- api_tests/direct_api_curl_test_hyperfinne0.sh
+++ api_tests/direct_api_curl_test_hyperfinne0.sh
@@ -1,4 +1,4 @@
-echo "curl 'http://127.0.0.1:8541/api/v3/community/list?sort=New&limit=50' | jq '.communities | length' | awk '\$1==50' | grep ." > runme0.sh
-chmod +x runme0.sh
+# echo "curl 'http://127.0.0.1:8541/api/v3/community/list?sort=New&limit=50' | jq '.communities | length' | awk '\$1==50' | grep ." > runme0.sh
+# chmod +x runme0.sh
 
 hyperfine 'yes "" | head -n200 | parallel -j 200 ./direct_api_curl_test_postlist0.sh'
